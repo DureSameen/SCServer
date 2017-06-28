038ce03f106f95e2758e7749b3fed0bfedcc7ce0
DureSameen <dmeena_pk@yahoo.com>
2017-06-28 19:45:04 +0500

    Customer controller

--- SCServer.Api/App_Start/WebApiConfig.cs
+++ SCServer.Api/App_Start/WebApiConfig.cs
@@ -1,7 +1,16 @@
-﻿using System;
+﻿using SCServer.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using Microsoft.Practices.Unity;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using SCServer.Api.Infrastructure;
+using System.Web.Http.Cors;
 
 namespace SCServer.Api
 {
@@ -10,6 +19,24 @@ namespace SCServer.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+        
+
+            var container = IoC.Container;
+            container.RegisterInstance<IHttpControllerActivator>(new UnityHttpControllerActivator(container));
+            config.Services.Replace(typeof(IHttpControllerActivator), new UnityHttpControllerActivator(container));
+
+            // Web API configuration and services
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            // Enabling Cors
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
+            GlobalConfiguration.Configuration.DependencyResolver = config.DependencyResolver;
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ SCServer.Api/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using SCServer.Core.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SCServer.Api.Controllers
+{
+    public class CustomerController : ApiController
+    {  
+        
+        
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public IList<Core.Dto.Customer> Get()
+        {
+            IList<Core.Dto.Customer> customers = _customerService.GetAll();
+
+            return customers;
+        }
+
+        public Core.Dto.Customer Get(Guid id)
+        {
+            return _customerService.Get(id);
+        }
+
+        public Core.Dto.Customer Post(Core.Dto.Customer customer)
+        {
+            return _customerService.Create(customer);
+        }
+
+        public Core.Dto.Customer Put(Core.Dto.Customer customer)
+        {
+            return _customerService.Update(customer);
+        }
+
+        public void Delete(Guid id)
+        {
+            _customerService.Delete(id);
+        }
+    }
+}
